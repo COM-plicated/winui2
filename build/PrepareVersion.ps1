d2cec481554115a3fb5d05e47c2209a7879503a1
Lili González <lili@guerra24.net>
2026-09-08 23:15:49 -0600

    Get subversion from the tag itself

--- build/PrepareVersion.ps1
+++ build/PrepareVersion.ps1
@@ -1,5 +1,11 @@
+[CmdLetBinding()]
+Param(
+    [string]$SubVersion
+)
+
 $pstZone = [System.TimeZoneInfo]::FindSystemTimeZoneById("Pacific Standard Time")
 $pstTime = [System.TimeZoneInfo]::ConvertTimeFromUtc((Get-Date).ToUniversalTime(), $pstZone)
 
+$env:SubVersion = $SubVersion
 $env:MUXVersionBuild = ($pstTime).ToString("yyMM")
-$env:MUXVersionRevision = ($pstTime).ToString("dd") + "001"
+$env:MUXVersionRevision = ($pstTime).ToString("dd") + $SubVersion
